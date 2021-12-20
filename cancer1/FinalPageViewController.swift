3226ebf7e8e317c8b65dc7df2db54b20c0fd205f
daniel <daniel.shchebalev@vhhscougars.org>
2021-12-20 09:25:56 -0600

    final commit, i added a last pager to tell the results of your test.

---
+++ cancer1/FinalPageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FinalPageViewController.swift
+//  cancer1
+//
+//  Created by Daniel Shchebalev on 12/14/21.
+//
+
+import Foundation
---
+++ cancer1/FinalViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FinalViewController.swift
+//  cancer1
+//
+//  Created by Daniel Shchebalev on 12/14/21.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+    
+   
+        // Do any additional setup after loading the view.
+    }
+
